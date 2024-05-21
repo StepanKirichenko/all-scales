2cc304a3211b401fb39bbca151753b90c2103d17
StepanKirichenko <kirichenkostepan@yandex.ru>
2024-05-21 23:23:26 +0300

    chore: add prettier

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,11 +1,11 @@
-import "./App.css";
+import './App.css'
 
 function App() {
-  return (
-    <div class="app">
-      <p>The piano will be here soon</p>
-    </div>
-  );
+    return (
+        <div class="app">
+            <p>The piano will be here soon</p>
+        </div>
+    )
 }
 
-export default App;
+export default App
